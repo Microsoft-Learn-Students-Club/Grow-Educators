1a11946b599907a221ad851086f699ededbace04
Adarsh Gupta <csedsadarsh@gmail.com>
2024-04-13 21:49:15 +0530

    changes done in feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -53,50 +53,57 @@ export default function Feedback() {
 
     return (
         <>
-            <div className="bg-blue-950 border-2 p-8 rounded-lg max-w-3xl mx-auto mt-10 mb-36">
-                <h1 className="text-3xl sm:text-4xl text-yellow-400 font-bold text-center mb-8">Feedback</h1>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <div>
-                            <label htmlFor="feedbackFormFirstName" className="block text-white font-semibold pb-1">First Name</label>
-                            <input value={firstName} onChange={e => setFirstName(e.target.value)} id="feedbackFormFirstName" type="text" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="First name" />
+            <div className="flex justify-center items-center pl-20">
+                <img
+                    src="https://img-cdn.pixlr.com/image-generator/history/6619772f6837fffe56e06b9d/6e6d3909-ebee-4c02-becf-c834bf187b4e/medium.webp"
+                    alt="Feedback image"
+                    className="hidden sm:block w-1/2 h-1/5 object-cover rounded-lg shadow-lg hover:shadow-2xl transition duration-300"
+                />
+                <div className="bg-blue-950 border-2 p-8 sm:p-12 rounded-lg max-w-sm sm:max-w-3xl mx-auto mt-10 mb-36 ">
+                    <h1 className="text-3xl sm:text-4xl text-yellow-400 font-bold text-center mb-8">Feedback</h1>
+                    <form onSubmit={handleSubmit} className="space-y-3">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                            <div>
+                                <label htmlFor="feedbackFormFirstName" className="block text-white font-semibold pb-1">First Name</label>
+                                <input value={firstName} onChange={e => setFirstName(e.target.value)} id="feedbackFormFirstName" type="text" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="First name" />
+                            </div>
+                            <div>
+                                <label htmlFor="feedbackFormLastName" className="block text-white font-semibold pb-1">Last Name</label>
+                                <input value={lastName} onChange={e => setLastName(e.target.value)} id="feedbackFormLastName" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Last Name" />
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor="feedbackFormLastName" className="block text-white font-semibold pb-1">Last Name</label>
-                            <input value={lastName} onChange={e => setLastName(e.target.value)} id="feedbackFormLastName" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Last Name" />
+
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                            <div>
+                                <label htmlFor="feedbackFormPhone" className="block text-white font-semibold pb-1">Mobile Number</label>
+                                <input value={mobileNumber} onChange={e => setMobileNumber(e.target.value)} id="feedbackFormPhone" type="text" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Enter 10-digit Mobile No." />
+                            </div>
+                            <div>
+                                <label htmlFor="feedbackFormClass" className="block text-white font-semibold pb-1">Class</label>
+                                <input type="text" value={classValue} onChange={e => setClassValue(e.target.value)} id="feedbackFormClass" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Select Class" />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                         <div>
-                            <label htmlFor="feedbackFormPhone" className="block text-white font-semibold pb-1">Mobile Number</label>
-                            <input value={mobileNumber} onChange={e => setMobileNumber(e.target.value)} id="feedbackFormPhone" type="text" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Enter 10-digit Mobile No." />
+                            <label htmlFor="feedbackFormGoal" className="block text-white font-semibold pb-1">Goal</label>
+                            <select id="feedbackFormGoal" className="w-full rounded-2xl border border-none focus:border-blue-500 px-4 py-2  shadow-md shadow-gray-900">
+                                <option value="6">Select Your Goal</option>
+                                <option value="5">School</option>
+                                <option value="4">NEET</option>
+                                <option value="3">JEE</option>
+                                <option value="2">MHT-CET</option>
+                                <option value="1">Tech</option>
+                            </select>
                         </div>
+
                         <div>
-                            <label htmlFor="feedbackFormClass" className="block text-white font-semibold pb-1">Class</label>
-                            <input type="text" value={classValue} onChange={e => setClassValue(e.target.value)} id="feedbackFormClass" className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Select Class" />
+                            <label htmlFor="feedbackFormMessage" className="block text-white font-semibold pb-1">Message</label>
+                            <textarea id="feedbackFormMessage" rows={3} className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Your message"></textarea>
                         </div>
-                    </div>
 
-                    <div>
-                        <label htmlFor="feedbackFormGoal" className="block text-white font-semibold pb-1">Goal</label>
-                        <select id="feedbackFormGoal" className="w-full rounded-2xl border border-none focus:border-blue-500 px-4 py-2  shadow-md shadow-gray-900">
-                            <option value="6">Select Your Goal</option>
-                            <option value="5">School</option>
-                            <option value="4">NEET</option>
-                            <option value="3">JEE</option>
-                            <option value="2">MHT-CET</option>
-                            <option value="1">Tech</option>
-                        </select>
-                    </div>
-
-                    <div>
-                        <label htmlFor="feedbackFormMessage" className="block text-white font-semibold pb-1">Message</label>
-                        <textarea id="feedbackFormMessage" rows={3} className="w-full rounded-lg border border-0.2 focus:border-blue-500 px-4 py-2 shadow-md shadow-gray-900" placeholder="Your message"></textarea>
-                    </div>
-
-                    <button type="submit" value={message} onChange={e => setMessage(e.target.value)} rows={3} className="bg-yellow-400 w-full font-semibold py-2 rounded-lg hover:bg-yellow-200 transition duration-300">Submit Feedback</button>
-                </form>
+                        <button type="submit" value={message} onChange={e => setMessage(e.target.value)} rows={3} className="bg-yellow-400 w-full font-semibold py-2 rounded-lg hover:bg-yellow-200 transition duration-300">Submit Feedback</button>
+                    </form>
+                </div>
             </div>
         </>
     );
